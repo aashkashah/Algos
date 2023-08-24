8b55044b9ab20def443454fec6889e4ddee78db3
aashka <aashka88@gmail.com>
2023-08-24 08:05:52 -0700

    add

--- Algos/TwoPointers/SameDirection.cs
+++ Algos/TwoPointers/SameDirection.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using static Algos.LinkedList;
 
 namespace Algos.TwoPointers
 {
-    class SameDirection
+    public class SameDirection
     {
+        public int RemoveDuplicates(List<int> arrList)
+        {
+            int slow = 0;
+            for(int fast = 0; fast < arrList.Count; fast++)
+            {
+                if(arrList[slow] != arrList[fast])
+                {
+                    slow++;
+                    arrList[slow] = arrList[fast];
+                }
+            }
+            return slow + 1;
+        }
+
+        public void Main()
+        {
+            ListNode node = LinkedList.CreateLinkedList();
+
+        }
     }
 }
