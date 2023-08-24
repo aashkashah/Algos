a4fe6d50407ef5b2e1fb2a55e2662a8b02a8cceb
Aashka Shah <aashah@microsoft.com>
2023-08-24 10:56:07 -0700

    algo.monster start

---
+++ Algos/TwoPointers/SameDirection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algos.TwoPointers
+{
+    public class SameDirection
+    {
+        public int RemoveDuplicates()
+        {
+            return 0;
+        }
+
+        public void Main()
+        {
+
+        }
+    }
+}
