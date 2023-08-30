61565d304ee2ca67c88dd5523e00499d4e6efd60
Aashka Shah <aashah@microsoft.com>
2023-08-29 18:23:59 -0700

    two pointers method

---
+++ Algos/TwoPointers/OppositeDirection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algos.TwoPointers
+{
+    public class OppositeDirection
+    {
+        public List<int> TwoSumSorted(List<int> arr, int target)
+        {
+            int l = 0;
+            int r = arr.Count - 1;
+
+            while(l < r)
+            {
+                int twoSum = arr[l] + arr[r];
+                if(twoSum == target)
+                {
+                    return new List<int> { l, r };
+                } 
+                else if(twoSum < target)
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+            return null;
+        }
+
+        public bool IsPalindrome(String str)
+        {
+            char[] arr = str.ToCharArray();
+            int l = 0;
+            int r = arr.Length - 1;
+
+            while(l < r) 
+            {
+                while(l < r && !Char.IsLetterOrDigit(arr[l]))
+                {
+                    l++;
+                }
+                while(l<r && !Char.IsLetterOrDigit(arr[r]))
+                {
+                    r++;
+                }
+
+                if (Char.ToLower(arr[l]) != Char.ToLower(arr[r])) 
+                {
+                    return false;
+                }
+                l++; r++;
+            }
+
+            return true;
+        }
+
+        public void Main()
+        {
+
+        }
+    }
+}
--- Algos/TwoPointers/SameDirection.cs
+++ Algos/TwoPointers/SameDirection.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using static Algos.LinkedList;
 
 namespace Algos.TwoPointers
 {
     public class SameDirection
     {
-        public int RemoveDuplicates()
+        public int RemoveDuplicates(List<int> arr)
         {
+            int slow = 0;
+            for(int fast = 0; fast < arr.Count; fast++)
+            {
+                if (arr[fast] != arr[slow])
+                {
+                    slow++;
+                    arr[slow] = arr[fast];
+                }
+            }
+            return slow + 1; ;
+        }
+
+        public int MiddleOfList(ListNode node)
+        {
+            ListNode slow = node;
+            ListNode fast = node;
+
+            while(fast != null  && fast.next != null) 
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            return slow.data;
+        }
+
+        public int MoveZeros(List<int> arr)
+        {
+            int slow = 0;
+            for(int fast = 0; fast < arr.Count; fast ++) 
+            {
+                if (arr[fast] != 0)
+                {
+                    int temp = arr[slow];
+                    arr[slow] = arr[fast];
+                    arr[fast] = temp;
+                    slow++;
+                }
+            }
             return 0;
         }
 
